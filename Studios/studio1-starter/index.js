c00adc609316ffcd1f88fd796e20b9567b341c7e
diotimaiscoding <74460793+diotimaiscoding@users.noreply.github.com>
2023-07-26 22:02:40 +0300

    Add files via upload

---
+++ Studios/studio1-starter/index.js
@@ -0,0 +1,60 @@
+import { createApp, Vue } from "https://mavue.mavo.io/mavue.js";
+
+let app = createApp({
+	data: {
+		tasks: [
+			{
+				done: false,
+				title: "",
+				active: true,
+			}
+		]
+	},
+
+	methods: {
+		addItem() {
+			this.tasks.push({});
+
+			this.setActive();
+		},
+
+		deleteItem(i) {
+			this.tasks.splice(i, 1);
+			this.setActive(i - 1);
+		},
+
+		clearCompleted () {
+			this.tasks = this.tasks.filter(task => !task.done);
+		},
+
+		setActive(i) {
+			for (let task of this.tasks) {
+				task.active = false;
+			}
+
+			if (i >= 0 && this.tasks[i]) {
+				this.tasks[i].active = true;
+			}
+			else if (this.tasks.length > 0) {
+				this.tasks.at(-1).active = true;
+			}
+		},
+
+        calculateDone() {
+            return this.tasks.filter(task => task.done).length;
+        }
+	},
+
+    watch: {
+        tasks: {
+            handler(newTasks) {
+                localStorage.setItem("tasks", JSON.stringify(newTasks));
+            },
+
+            deep: true
+        }
+    },
+}, "#app");
+
+// For debugging
+globalThis.app = app;
